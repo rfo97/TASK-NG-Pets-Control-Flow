0c743e593196733e8e6700b41a23a4360fa35986
rfo97 <rfa.alobaid@gmail.com>
2025-04-07 10:36:13 +0300

    fix: Added forloop and if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 import pets from '../data/pets';
 import { PetComponent } from './pet/pet.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [PetComponent],
+  imports: [PetComponent, HighlightDirective],
 })
 export class AppComponent {
   title = 'TASK-NG-Pets-Directives';
